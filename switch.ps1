54c13e0c3260020b89e46c208e8f561e0287c75f
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-04-14 10:48:32 -0700

    Whitespace

--- switch.ps1
+++ switch.ps1
@@ -29,62 +29,63 @@ param(
 )
 
 function restart_httpd {
-	Get-WmiObject win32_service |
-	  Where-Object {
-		  $_.Name -like 'Apache2.4' -and $_.StartMode -eq 'Auto'
-	  } | Stop-Service;
+    Get-WmiObject win32_service |
+        Where-Object {
+        $_.Name -like 'Apache2.4' -and $_.StartMode -eq 'Auto'
+    } | Stop-Service;
 
-	<# Kill httpd.exe for sbt3-9400, its running as console app outside service #>
-	Get-Process | Where-Object { $_.Name -like 'httpd' } | Stop-Process -Force;
+    <# Kill httpd.exe for sbt3-9400, its running as console app outside service #>
+    Get-Process | Where-Object { $_.Name -like 'httpd' } | Stop-Process -Force;
 
-	Get-WmiObject win32_service |
-	  Where-Object {
-		  $_.Name -like 'Apache2.4' -and $_.StartMode -eq 'Auto'
-	  } | Start-Service;
+    Get-WmiObject win32_service |
+        Where-Object {
+        $_.Name -like 'Apache2.4' -and $_.StartMode -eq 'Auto'
+    } | Start-Service;
 
-	<# Start apache from shortcut for 9400 #>
-	$glob = "${env:SYSTEMDRIVE}/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/Apache HTTP Server.lnk";
-	$httpd_link = Get-ChildItem $glob -ea 0 | Select-Object -Last 1 | Select-Object -exp fullname;
-	if($httpd_link -ne $null) {
-		Invoke-Item $httpd_link
-	}
+    <# Start apache from shortcut for 9400 #>
+    $glob = "${env:SYSTEMDRIVE}/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/Apache HTTP Server.lnk";
+    $httpd_link = Get-ChildItem $glob -ea 0 | Select-Object -Last 1 | Select-Object -exp fullname;
+    if ($httpd_link -ne $null) {
+        Invoke-Item $httpd_link
+    }
 }
 
-if($prod){
-	<# Production settings #>
+if ($prod) {
+    <# Production settings #>
 
-	Copy-Item c:/php/php.ini c:/php/php.ini.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).ini;
+    Copy-Item c:/php/php.ini c:/php/php.ini.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).ini;
 
-	(Get-Content c:/php/php.ini) `
-	  -replace '^date.timezone =.*', ';date.timezone =' `
-	  -replace '^error_log =.*', ';error_log = php_errors.log' `
-	  -replace '^display_errors =.*', 'display_errors = Off' `
-	  -replace '^display_startup_errors =.*', 'display_startup_errors = Off' `
-	  -replace '^\s*;?\s*log_errors\s*=.*', 'log_errors = On' `
-	  -replace '^error_reporting = E_ALL', 'error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT' | Set-Content c:/php/php.ini;
+    (Get-Content c:/php/php.ini) `
+ -replace '^date.timezone =.*', ';date.timezone =' `
+ -replace '^error_log =.*', ';error_log = php_errors.log' `
+ -replace '^display_errors =.*', 'display_errors = Off' `
+ -replace '^display_startup_errors =.*', 'display_startup_errors = Off' `
+ -replace '^\s*;?\s*log_errors\s*=.*', 'log_errors = On' `
+ -replace '^error_reporting = E_ALL', 'error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT' | Set-Content c:/php/php.ini;
 
-	Copy-Item c:/Apache/conf/httpd.conf c:/Apache/conf/httpd.conf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).conf;
+    Copy-Item c:/Apache/conf/httpd.conf c:/Apache/conf/httpd.conf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).conf;
 
-	(Get-Content c:/Apache/conf/httpd.conf) `
-	  -replace '^\s*LogLevel\s*(debug|info|notice|warn|error|crit|alert|emerg)', 'LogLevel emerg' | Set-Content c:/Apache/conf/httpd.conf;
+    (Get-Content c:/Apache/conf/httpd.conf) `
+ -replace '^\s*LogLevel\s*(debug|info|notice|warn|error|crit|alert|emerg)', 'LogLevel emerg' | Set-Content c:/Apache/conf/httpd.conf;
 
-} else {
-	<# Development/Test settings #>
+}
+else {
+    <# Development/Test settings #>
 
-	Copy-Item c:/php/php.ini c:/php/php.ini.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).ini;
+    Copy-Item c:/php/php.ini c:/php/php.ini.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).ini;
 
-	(Get-Content c:/php/php.ini) `
-	  -replace '^\s*;?\s*date.timezone\s*=.*', 'date.timezone = America/Los_Angeles' `
-	  -replace '^\s*;?\s*error_log\s*=\s*.*', 'error_log = c:/Apache/logs/php_errors.log' `
-	  -replace '^\s*;?\s*display_errors\s*=.*', 'display_errors = On' `
-	  -replace '^\s*;?\s*display_startup_errors\s*=.*', 'display_startup_errors = On' `
-	  -replace '^\s*;?\s*log_errors\s*=.*', 'log_errors = On' `
-	  -replace '^\s*;?\s*error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT', 'error_reporting = E_ALL' | Set-Content -Path c:/php/php.ini;
+    (Get-Content c:/php/php.ini) `
+ -replace '^\s*;?\s*date.timezone\s*=.*', 'date.timezone = America/Los_Angeles' `
+ -replace '^\s*;?\s*error_log\s*=\s*.*', 'error_log = c:/Apache/logs/php_errors.log' `
+ -replace '^\s*;?\s*display_errors\s*=.*', 'display_errors = On' `
+ -replace '^\s*;?\s*display_startup_errors\s*=.*', 'display_startup_errors = On' `
+ -replace '^\s*;?\s*log_errors\s*=.*', 'log_errors = On' `
+ -replace '^\s*;?\s*error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT', 'error_reporting = E_ALL' | Set-Content -Path c:/php/php.ini;
 
-	Copy-Item c:/Apache/conf/httpd.conf c:/Apache/conf/httpd.conf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).conf;
+    Copy-Item c:/Apache/conf/httpd.conf c:/Apache/conf/httpd.conf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).conf;
 
-	(Get-Content c:/Apache/conf/httpd.conf) `
-	  -replace '^\s*LogLevel\s*(debug|info|notice|warn|error|crit|alert|emerg)', 'LogLevel debug' | Set-Content c:/Apache/conf/httpd.conf;
+    (Get-Content c:/Apache/conf/httpd.conf) `
+ -replace '^\s*LogLevel\s*(debug|info|notice|warn|error|crit|alert|emerg)', 'LogLevel debug' | Set-Content c:/Apache/conf/httpd.conf;
 }
 
 restart_httpd
